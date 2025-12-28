91529b21c8f6812b194c249d03230361e88cbc82
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2025-12-28 21:08:05 +0200

    Added the Model classes for the game

---
+++ src/models/LetterResult.ts
@@ -0,0 +1,31 @@
+import LetterState from "./LetterState.ts";
+
+class GameModel {
+    public readonly letter: string;
+    public readonly position: number;
+    public readonly state: typeof LetterState[keyof typeof LetterState];
+
+    constructor(letter: string, position: number, state: typeof LetterState[keyof typeof LetterState]) {
+        this.letter = letter;
+        this.position = position;
+        this.state = state;
+    }
+
+    public isInPlace() {
+        return this.state === LetterState.IN_PLACE;
+    }
+
+    public isCorrect() {
+        return this.state === LetterState.CORRECT;
+    }
+
+    public isWrong() {
+        return this.state === LetterState.WRONG;
+    }
+
+    public isEmpty() {
+        return this.state === LetterState.EMPTY;
+    }
+}
+
+export default GameModel;
---
+++ src/models/LetterState.ts
@@ -0,0 +1,8 @@
+const LetterState = {
+    IN_PLACE: 'inplace',
+    CORRECT: 'correct',
+    WRONG: 'wrong',
+    EMPTY: 'empty'
+} as const;
+
+export default LetterState;
